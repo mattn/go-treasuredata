81652760bc49ff4253cde44c5846ca07bec0148a
mattn <mattn.jp@gmail.com>
2013-10-21 22:53:37 +0900

    Toolize

--- example/example.go
+++ example/example.go
@@ -1,60 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mattn/go-treasuredata"
 	"log"
 	"os"
 )
 
+var info = flag.Bool("i", false, "information of databases")
+var db = flag.String("d", "", "database")
+var query = flag.String("q", "", "query string")
+
 func main() {
+	flag.Parse()
+
 	client := treasuredata.NewClient(os.Getenv("TREASURE_DATA_API_KEY"))
 	databases, err := client.DatabaseList()
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, database := range databases {
-		fmt.Println(database.Name)
-		fmt.Println(database.Count)
-		fmt.Println(database.CreatedAt.String())
 
-		tables, err := client.TableList(database.Name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, table := range tables {
-			fmt.Println(table.Id)
-			fmt.Println(table.Name)
-			for _, col := range table.Schema.Columns() {
-				fmt.Println(col.Name, col.Type)
+	if *info || *db == "" || *query == "" {
+		for _, database := range databases {
+			fmt.Println("DATABASE:", database.Name)
+			fmt.Println("  Record Count:", database.Count)
+			fmt.Println("  Created At:", database.CreatedAt.String())
+
+			tables, err := client.TableList(database.Name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println()
+			for _, table := range tables {
+				fmt.Println("  TABLE:", table.Id, table.Name)
+				for _, col := range table.Schema.Columns() {
+					fmt.Println("    COLUMN:", col.Name, col.Type)
+				}
 			}
 		}
-	}
-
-	job, err := client.JobIssueHive("mattn", "select * from unko", 2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Waiing Job
-	/*
-	for {
-		status, err := client.JobStatus(job.JobId)
+	} else {
+		job, err := client.JobIssueHive(*db, *query)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if status.Status != "running" {
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-	*/
 
-	err = client.JobResult(job.JobId, func(line string) error {
-		fmt.Println(line)
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
+		err = client.JobResult(job.JobId, func(line string) error {
+			fmt.Println(line)
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
